4747c47d0d1e2c15fc973a5e2e32282c13b51f39
woolfik-27 <vladkutas4@gmail.com>
2024-11-17 18:49:06 +0300

    Создание наследуемой формы

---
+++ WindowsFormsApp1/nForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class nForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Text = "Hello!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // nForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "nForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
